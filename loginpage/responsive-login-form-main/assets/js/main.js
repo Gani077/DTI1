5d63ae0b79a2deaaddfccd54725bba0c219f4be2
Ganesh <140585754+Gani077@users.noreply.github.com>
2025-02-20 22:32:18 +0530

    Update main.js

--- loginpage/responsive-login-form-main/assets/js/main.js
+++ loginpage/responsive-login-form-main/assets/js/main.js
@@ -1,30 +1,17 @@
-/*=============== SHOW HIDDEN - PASSWORD ===============*/
 const showHiddenPassword = (inputPassword, inputIcon) => {
   const input = document.getElementById(inputPassword),
         iconEye = document.getElementById(inputIcon)
 
-  iconEye.addEventListener('click', () => {
-    // Change password to text
-    if (input.type === 'password') {
-      // Switch to text
-      input.type = 'text'
-
-      // Add icon
-      iconEye.classList.add('ri-eye-line')
-
-      // Remove icon
+  iconEye.addEventListener('click', () => { 
+    if (input.type === 'password') { 
+      input.type = 'text' 
+      iconEye.classList.add('ri-eye-line') 
       iconEye.classList.remove('ri-eye-off-line')
-    } else {
-      // Change to password
-      input.type = 'password'
-
-      // Remove icon
-      iconEye.classList.remove('ri-eye-line')
-
-      // Add icon
+    } else { 
+      input.type = 'password' 
+      iconEye.classList.remove('ri-eye-line') 
       iconEye.classList.add('ri-eye-off-line')
     }
   })
-}
-
+} 
 showHiddenPassword('password', 'input-icon')
